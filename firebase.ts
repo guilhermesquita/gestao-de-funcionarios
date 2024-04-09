7bf668e0f180935bffb9d40e6497f0eee34e1f6b
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-04-08 21:26:05 -0400

    chore: config firebase and firestore

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuzyyAfk50wAYkKTz9PdYmIOIj_9cQDL8",
+  authDomain: "gestao-de-funcionarios-e56ed.firebaseapp.com",
+  projectId: "gestao-de-funcionarios-e56ed",
+  storageBucket: "gestao-de-funcionarios-e56ed.appspot.com",
+  messagingSenderId: "245514740424",
+  appId: "1:245514740424:web:3c8d5ce2ccf2e08a4ffbfd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

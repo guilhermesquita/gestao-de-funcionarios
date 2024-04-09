a4c82319de66513a884f23770806cee51cf36012
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-04-08 21:24:46 -0400

    chore: config firebase and firestore

---
+++ src/infra/repos/firebase/helpers/connection.ts
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuzyyAfk50wAYkKTz9PdYmIOIj_9cQDL8",
+  authDomain: "gestao-de-funcionarios-e56ed.firebaseapp.com",
+  projectId: "gestao-de-funcionarios-e56ed",
+  storageBucket: "gestao-de-funcionarios-e56ed.appspot.com",
+  messagingSenderId: "245514740424",
+  appId: "1:245514740424:web:3c8d5ce2ccf2e08a4ffbfd"
+};
+
+const serviceAccount = require("../../../../../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  ...firebaseConfig // Spread operator para adicionar configurações adicionais
+});
+
+// Obtenha uma referência ao Firestore
+export const db = admin.firestore();
+
+// Exporte o Firestore
+module.exports = { db };
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,23 +1,31 @@
-import './config/module-alias'
-import { PgConnection } from '@/infra/repos/postgres/helpers'
-import 'reflect-metadata'
-import { env } from '@/main/config/env'
-import { API, SWAGGER } from '@/utils/constants'
-import { Request, Response } from 'express'
+import express, { Request, Response } from "express";
+import { db } from "../infra/repos/firebase/helpers";
+  
+  // Express
+  const app = express();
+  
+  app.use(express.json());
+  
+  app.listen(Number(process.env.PORT || 3005), () => {
+      console.log(`Servidor rodando na porta ${process.env.PORT}`);
+  });
 
-PgConnection.getInstance()
-  .connect()
-  .initialize()
-  .then(async () => {
-    const { app } = await import('@/main/config/app')
+app.get('/Ping', (req: Request, res: Response) => {
+  res.send('pong')
+})
 
-    app.listen(env.port, () => {
-      console.log(`Server running at http://localhost:${env.port}${API}`)
-      console.log(`Swagger at http://localhost:${env.port}${API}${SWAGGER}`)
-    })
+app.get('/test', async (req: Request, res: Response) => {
+  try {
+    // Aguarde a resolução da Promise
+    const snapshot = await db.collection('teste').get();
 
-    app.get('/Ping', (req: Request, res: Response) => {
-      res.send('Pong')
-  })
-  })
-  .catch(console.error)
+    // Mapeie os dados do snapshot para um array
+    const data = snapshot.docs.map(doc => doc.data());
+
+    // Envie os dados como resposta
+    res.json(data)
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    res.status(500).send('Erro ao buscar dados');
+  }
+});
